ffcf16e59b9c0848cb87acd730a5c4b68f0b3722
Mdmosley12 <mdmosley12@gmail.com>
2023-01-19 11:45:08 +0000

    add comment_count column

--- db/models/model.js
+++ db/models/model.js
@@ -21,7 +21,14 @@ const selectAllArticles = () => {
 };
 const selectArticleById = (article_id) => {
     const queryValues = [article_id];
-    const queryString = `SELECT * FROM articles WHERE article_id = $1;`;
+    const queryString = `SELECT articles.*, COUNT(comments.article_id)
+     AS comment_count
+     FROM articles
+     LEFT JOIN comments
+     ON articles.article_id=comments.article_id
+     WHERE articles.article_id = $1
+     GROUP BY articles.article_id
+     ORDER BY created_at DESC;`;
     return db.query(queryString, queryValues).then((result) => {
         if (result.rows.length === 0) {
             return Promise.reject({ status: 404, msg: 'Requested article not found!'})
