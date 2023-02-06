081f952da096b5e9e4a89e47e395587ce2be13f4
Mdmosley12 <mdmosley12@gmail.com>
2023-02-06 10:19:19 +0000

    allow cross origin resource sharing

--- db/app/app.js
+++ db/app/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const apiRouter = require('../routes/api-router');
+const cors = require('cors');
 
+app.use(cors())
 app.use(express.json());
 app.use('/api', apiRouter);
 
